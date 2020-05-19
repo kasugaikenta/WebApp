71e69ed48813b5072dba16e8c373fa73fd811cc9
EC2 Default User <ec2-user@ip-172-31-38-233.ap-northeast-1.compute.internal>
2020-05-19 07:40:04 +0000

    岩本変更分ver.8

--- questionanswer/app/Http/Controllers/QuestionsController.php
+++ questionanswer/app/Http/Controllers/QuestionsController.php
@@ -17,14 +17,8 @@ class QuestionsController extends Controller
     }
     
     public function sendQuestion(Request $request){
-<<<<<<< HEAD
         //Validatorを使って入力された値のチェック(バリデーション)処理
         $validator = Validator::make($request->all() , ['question_content' => 'required|max:1000', ]);
-=======
-        //Validatorを使って入力された値のチェック(バリデーション)処理　（今回は255以上と空欄の場合エラーになります）
-        $validator = Validator::make($request->all() , ['question_content' => 'required|max:1000', ])
-        $validator = Validator::make($request->all() , ['question' => 'required|max:1000', ]);
->>>>>>> d67f5d1d0d4f14c6f447133e025891783344997b
 
         //バリデーションの結果がエラーの場合
         if ($validator->fails())

6d28079b9e73634326ffb5008af68baf3e728932
EC2 Default User <ec2-user@ip-172-31-37-28.ap-northeast-1.compute.internal>
2020-05-21 06:19:15 +0000

    ユーザ詳細画面編集

--- questionanswer/resources/views/user/show.blade.php
+++ questionanswer/resources/views/user/show.blade.php
@@ -4,44 +4,44 @@
 
   <!-- カードの詳細情報-->
   {{csrf_field()}} 
-    <div >
-      <div >
-        <div>
-          <div class="col-sm-3 control-label">
+    <div>
+      <div>
+        <div class="text-center ">
+          <div class="col-md-12 control-label" style="margin-top:30px;">
             <label>名前</label>
           </div>
-          <div class="col-sm-6">
+          <div class="col-md-12 " style="margin-bottom:30px;">
             <p>{{$user->name}}</p>
           </div>
         </div>
-        <div>
-          <div class="col-sm-3 control-label">
+        <div class="text-center">
+          <div class="col-md-12 control-label">
             <label>メールアドレス</label>
           </div>
-          <div class="col-sm-6">
+          <div class="col-md-12" style="margin-bottom:30px;">
             <p>{{$user->email}}</p>
           </div>
         </div>
-        <div>
-          <div class="col-sm-3 control-label">
+        <div class="text-center">
+          <div class="col-md-12 control-label">
             <label>パスワード</label>
           </div>
-          <div class="col-sm-6">
-            <p>****</p>
+          <div class="col-md-12" style="margin-bottom:30px;">
+            <p>********</p>
           </div>
         </div>
-        <div>
-          <div class="col-sm-3 control-label">
+        <div class="text-center">
+          <div class="col-md-12 control-label">
             <label>登録日時</label>
           </div>
-          <div class="col-sm-6">
+          <div class="col-md-12 " style="margin-bottom:30px;">
             <p>{{$user->created_at}}</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="form-group"> 
-      <div class="col-sm-offset-3 col-sm-6 text-center"> 
+    <div class="form-group">
+      <div class=" col-md-12 text-center"> 
         <a class="cardDetail_link" href="/user/edit">
           <button class="btn btn-primary"><i class="glyphicon glyphicon-saved"></i>&nbsp;&nbsp;編集する</button>
         </a>
